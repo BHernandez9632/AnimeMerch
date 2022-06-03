a6614beaaf180f74e6de781e1ef50a74d340938b
Bryan <karmapup32@gmail.com>
2022-06-02 23:03:32 -0400

    AnimeMerch-Store

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> Anime Merch </a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;
