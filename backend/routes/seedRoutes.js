cc5c3ef69c17dd7a6493dfb8bf1693cbcb7c5ab8
Bryan <karmapup32@gmail.com>
2022-06-04 04:57:57 -0400

    users

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import Merch from '../models/merchModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
   await Merch.remove({});
   const createdMerchs = await Merch.insertMany(data.merchs);
-  res.send({ createdMerchs });
+  await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdMerchs, createdUsers });
 });
 
 export default seedRouter;
